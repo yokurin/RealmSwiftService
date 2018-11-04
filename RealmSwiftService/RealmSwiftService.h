9c406ffdec1a3a33e39536e5d1335764a323419f
Tsubasa Hayashi <yoku.rin.99@gmail.com>
2018-11-05 00:08:00 +0900

    Initial Commit

---
+++ RealmSwiftService/RealmSwiftService.h
@@ -0,0 +1,19 @@
+//
+//  RealmSwiftService.h
+//  RealmSwiftService
+//
+//  Created by Tsubasa Hayashi on 2018/11/05.
+//  Copyright © 2018 Tsubasa Hayashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmSwiftService.
+FOUNDATION_EXPORT double RealmSwiftServiceVersionNumber;
+
+//! Project version string for RealmSwiftService.
+FOUNDATION_EXPORT const unsigned char RealmSwiftServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmSwiftService/PublicHeader.h>
+
+
