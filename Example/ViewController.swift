5213dcb5b330c165d8b5c16895d7eeea3d77262e
Tsubasa Hayashi <yoku.rin.99@gmail.com>
2018-11-05 00:43:58 +0900

    Refactor Example

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwiftService
 
 class ViewController: UIViewController {
 
@@ -14,25 +15,27 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         //--- RealmAccessible Example ---
-        let cat = Cat(name: "tama")
-        Cat.all()
-        Cat.find(by: "id")
-        Cat.first()
-        Cat.last()
-        try! Cat.deleteAll()
-        try! cat.delete()
-        try! cat.save()
+        let cat = Cat(name: "tama")  // Create Realm Object
+        _ = Cat.all()                // Get All Object of Cat
+        _ = Cat.find(by: "id")       // Find Cat by id property
+        _ = Cat.first()              // Get Last Object of Cat
+        _ = Cat.last()               // Get First Object of Cat
+        try! cat.save()              // Save Object of Cat
+        try! cat.delete()            // Delete Object of Cat
+        try! Cat.deleteAll()         // Delete All Object of Cat
+
 
         // --- Dao Example ---
-        let dao = RealmDao<Dog>()
-        let dog = Dog(name: "pochi")
-        dao.all()
-        dao.find(by: "id")
-        dao.first()
-        dao.last()
-        try! dao.deleteAll()
-        try! dao.delete(dog)
-        try! dao.save(dog)
+        let dao = RealmDao<Dog>()    // Create Dao of Dog
+        let dog = Dog(name: "pochi") // Create Realm Object
+        _ = dao.all()                // Get All Object of Dog
+        _ = dao.find(by: "id")       // Find Dog by id property
+        _ = dao.first()              // Get First Object of Dog
+        _ = dao.last()               // Get Last Object of Dog
+        try! dao.save(dog)           // Save Object of Dog
+        try! dao.delete(dog)         // Delete Object of Dog
+        try! dao.deleteAll()         // Delete All Object of Dog
+
     }
 
 }
